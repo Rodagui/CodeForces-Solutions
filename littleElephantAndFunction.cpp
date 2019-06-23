8c0aa97f4b5bc16304c47b7974047c8cb3add760
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2019-06-22 21:48:33 -0500

    Add files via upload

---
+++ littleElephantAndFunction.cpp
@@ -0,0 +1,24 @@
+/*A. Little Elephant and Function*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n ;
+
+	cin >> n;
+
+	cout << n;
+
+	for (int i = 1; i < n; ++i)
+		cout << ' ' << i;
+	
+	cout << '\n';
+
+	return 0;
+}
