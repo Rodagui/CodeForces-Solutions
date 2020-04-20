89245d71baa33652c984596125af83d0e1ee7031
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-04-20 14:00:09 -0500

    Add files via upload

--- minutesBeforetheNewYear.cpp
+++ minutesBeforetheNewYear.cpp
@@ -1,37 +1,37 @@
 /*A. Minutes Before the New Year*/
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main(){
 	
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
+ 
 	int cases;
 	cin >> cases;
-
+ 
 	while(cases--){
 		
 		int hora, min;
 		
 		cin >> hora >> min;
-
+ 
 		int ans = 0;
 		int aux;
-
+ 
 		hora++;
-
+ 
 		aux = 60 - min;
-
+ 
 		ans+= aux;
-
+ 
 		aux = 24 - hora;
 		ans+= (60 * aux);
 			
 		cout << ans << '\n';
 	}
-
+ 
 	return 0;
 }
