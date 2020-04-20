be41d77cafb56c884715c081922614ba2ecc82fe
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-04-20 14:09:13 -0500

    Add files via upload

---
+++ removeProgression.cpp
@@ -0,0 +1,24 @@
+/*C - Remove a Progression*/
+#include <bits/stdc++.h>
+
+using namespace std;
+using Long = long long;
+
+int main(){
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int casos;
+	cin >> casos;
+
+	while(casos--){
+		int tam;
+		int pos;
+		cin >> tam >> pos;
+		cout << pos * 2 << '\n';
+	}
+
+	return 0;
+}
