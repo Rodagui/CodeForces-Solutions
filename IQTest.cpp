9f9966ab56d59bcd992fed90e1a7f2134ae67ca0
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-05-21 21:07:55 -0500

    Add files via upload

--- IQTest.cpp
+++ IQTest.cpp
@@ -1,309 +1,41 @@
-/*A. IQ Test*/
+/*D - IQ test*/
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main(){
 
-	char matriz[4][4];
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	for (int i = 0; i < 4; ++i)
-		for (int j = 0; j < 4; ++j)
-			cin >> matriz[i][j];
+	int tam;
 
-	
-	bool can = false;
-	int negro = 0, blanco = 0;
+	cin >> tam;
 
-	for (int i = 0; i < 4; ++i){
+	int pares = 0;
+	int impares = 0;
+	int impos = -1;
+	int parpos = -1;
+	int num;
 
-		for (int j = 0; j < 4; ++j){
-			
-			/*----- cuadro 1 ------*/
-			if( matriz[0][0] == '#' )
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[0][1] == '#' )
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][0] == '#' )
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][1] == '#' )
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-			/*----- cuadro 2 ------*/
-			if( matriz[0][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[0][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-
-
-			/*----- cuadro 3 ------*/
-			if( matriz[0][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[0][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-
-			/*----- cuadro 4 ------*/
-			if( matriz[1][0] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][0] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-
-			/*----- cuadro 5 ------*/
-			if( matriz[1][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-			/*----- cuadro 6 ------*/
-			if( matriz[1][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[1][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-			/*----- cuadro 7 ------*/
-			if( matriz[2][0] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][0] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-			/*----- cuadro 8 ------*/
-			if( matriz[2][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][1] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
-			/*----- cuadro 9 ------*/
-			if( matriz[2][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[2][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][2] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if( matriz[3][3] == '#')
-				negro++;
-			else
-				blanco++;
-
-			if(blanco >= 3 or negro >= 3){
-				can = true;
-				break;
-			}
-			else{
-				blanco = 0;
-				negro = 0;
-			}
-
+	for (int i = 0; i < tam; ++i){
+		cin >> num;
+		if(num % 2 == 0){
+			pares++;
+			parpos = i + 1;
 		}
-
-		if(can)
-			break;
+		else{
+			impares++;
+			impos = i + 1;
+		}
 	}
 
-	if(can)
-		cout << "YES\n";
+	if(impares == 1)
+		cout << impos;
 	else
-		cout << "NO\n";
+		cout << parpos;
+	
 
 	return 0;
 }
