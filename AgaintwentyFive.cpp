c03862b0f56e73f1ba71594f9583b2e09e8e47bc
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2025-02-25 17:30:33 -0600

    Add files via upload

---
+++ AgaintwentyFive.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie();
+	cout.tie();
+
+	int pow;
+	cin >> pow;
+	cout << "25";
+	
+	return 0;
+}
